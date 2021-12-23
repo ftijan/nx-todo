c01859ec5f5b35240320f814b3534818dd910729
Filip Tijan <ftijan@gmail.com>
2021-12-23 23:00:45 +0000

    adding nestjs api and shared lib

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-
-interface Todo {
-  title: string;
-}
+import { HttpClient } from '@angular/common/http';
+import { Todo } from '@nx-todo/data';
 
 @Component({
   selector: 'nx-todo-root',
@@ -10,18 +8,19 @@ interface Todo {
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  todos: Todo[] = [
-    {
-      title: 'Todo 1'
-    },
-    {
-      title: 'Todo 2'
-    }
-  ];
+  todos: Todo[] = [];
+
+  constructor(private http: HttpClient) {
+    this.fetch();
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe((t) => (this.todos = t));
+  }
 
   addTodo() {
-    this.todos.push({
-      title: `New todo ${Math.floor(Math.random() * 1000)}`,
-    })
+    this.http.post('/api/addTodo', {}).subscribe(() => {
+      this.fetch();
+    });
   }
 }
